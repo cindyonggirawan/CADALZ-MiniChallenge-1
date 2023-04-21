e532149ba9ca28cebb543504ee5ef7d33aea8bae
Alfine <alfine10@gmail.com>
2023-04-21 12:57:49 +0700

    Add Generate challenge view (including components such as category capsule and challenge card), but still haven't based on core data

--- Playdate/Core/Utilities/CoreData/CoreDataManager.swift
+++ Playdate/Core/Utilities/CoreData/CoreDataManager.swift
@@ -5,4 +5,29 @@
 //  Created by Cindy Amanda Onggirawan on 19/04/23.
 //
 
-import Foundation
+import SwiftUI
+import CoreData
+
+class CoreDataManager {
+    static let instance = CoreDataManager()
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    init() {
+        container = NSPersistentContainer(name: "")
+        container.loadPersistentStores { desc, error in
+            if let error = error {
+                print("Error loading Core Data. \(error)")
+            }
+        }
+        context = container.viewContext
+    }
+    
+    func save() {
+        do {
+            try context.save()
+        } catch let error {
+            print("Error saving Core Data. \(error.localizedDescription)")
+        }
+    }
+}
---
+++ Playdate/Features/Challenge/Presenters/Views/GenerateChallengeView.swift
@@ -0,0 +1,77 @@
+//
+//  GenerateChallengeView.swift
+//  Playdate
+//
+//  Created by Alfine on 21/04/23.
+//
+
+import SwiftUI
+
+struct GenerateChallengeView: View {
+    var body: some View {
+        //TODO: BELUM RESPONSIVE
+        VStack {
+            //Title
+            VStack(alignment: .leading) {
+                //TODO: Nama user ambil dari coredata
+                HStack{
+                    Text("Hi Alfine,")
+                        //TODO: Font Family Poppins
+                        .font(.system(size: 16))
+                        .fontWeight(.regular)
+                        .foregroundColor(.primaryDarkGray)
+                        .padding(.bottom, -1)
+                    Spacer()
+                }
+                
+                Text("What challenge do you want for your next date?")
+                    //TODO: Font Family Poppins
+                    .font(.system(size: 22))
+                    .fontWeight(.medium)
+                    .foregroundColor(.black)
+            }
+            .padding(.bottom, 25)
+            .frame(maxWidth: 342)
+            
+            //Category Capsule
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack{
+                    //TODO: Set Category Value
+                    CategoryCapsuleView()
+                    CategoryCapsuleView()
+                    CategoryCapsuleView()
+                    CategoryCapsuleView()
+                }
+            }
+            .frame(maxWidth: 342)
+            .padding(.bottom, 20)
+            
+            
+            //Challenge Card
+            //TODO: Card ZStack View & Logic
+            ChallengeCardView()
+            
+            //Accept Button
+            Button(action: {
+                //TODO: Accept Action
+                
+            }, label: {
+                Text("Accept Challenge!")
+                    //TODO: Set Font Family "Poppins"
+                    .font(.system(size: 14))
+                    .fontWeight(.bold)
+            })
+            .buttonStyle(FixedSizeRoundedButtonStyle())
+            .padding(.top, 20)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.white)
+    }
+}
+
+struct GenerateChallengeView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenerateChallengeView()
+    }
+}
