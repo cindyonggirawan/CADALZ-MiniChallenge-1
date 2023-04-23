f0c5211d76f15beaa8210da435673fe9d77099d7
Louis Wijaya <70441261+louis-not@users.noreply.github.com>
2023-04-23 14:18:10 +0700

    add Submit Challenge Modal

---
+++ Playdate/Features/Challenge/Presenters/Views/ChallengeSubmitModal.swift
@@ -0,0 +1,20 @@
+//
+//  ChallengeSubmitModal.swift
+//  Playdate
+//
+//  Created by Louis Mayco Dillon Wijaya on 23/04/23.
+//
+
+import SwiftUI
+
+struct ChallengeSubmitModal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChallengeSubmitModal_Previews: PreviewProvider {
+    static var previews: some View {
+        ChallengeSubmitModal()
+    }
+}
