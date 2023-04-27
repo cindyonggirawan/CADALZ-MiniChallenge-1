c2e61dcc014b63b5e1e2068b3fe159e1849d7661
cindyonggirawan <cindy.onggirawan@binus.ac.id>
2023-04-27 10:01:22 +0700

    Works but isn't perfect: The timer runs even though the app is minimized, but still if the app is closed, the timer will reset to 1 day

--- Playdate/Features/Challenge/Presenters/Views/temporaryCountdownView.swift
+++ Playdate/Features/Challenge/Presenters/Views/temporaryCountdownView.swift
@@ -7,29 +7,48 @@
 
 import SwiftUI
 
+// MARK: - The timer runs even though the app is minimized, but still if the app is closed, the timer will reset to 1 day
+
+struct TimerView: View {
+    @State var nowDate: Date = Date()
+    
+    let setDate: Date
+    
+    var timer: Timer {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.nowDate = Date()
+        }
+    }
+    
+    var body: some View {
+        Text(TimerFunction(from: setDate))
+            .onAppear(perform: {
+                self.timer
+            })
+    }
+    
+    func TimerFunction(from date: Date) -> String {
+        let calendar = Calendar(identifier: .gregorian)
+        
+        let timeValue = calendar
+            .dateComponents([.day, .hour, .minute, .second], from: nowDate, to: setDate)
+        
+        return String(format: "%02d days left - %02d:%02d:%02d", timeValue.day!,timeValue.hour!, timeValue.minute!, timeValue.second!)
+    }
+}
+
 struct temporaryCountdownView: View {
-    @ObservedObject var stopwatch = Stopwatch()
+    var toDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
     
     var body: some View {
         VStack {
-            Text("Countdown: \(stopwatch.remainingSeconds / 3600) hours, \((stopwatch.remainingSeconds % 3600) / 60) minutes, \(stopwatch.remainingSeconds % 60) seconds")
-                .font(.headline)
-                .padding()
+            Text("Countdown for Challenge")
             
-            Text("Stopwatch: \(stopwatch.message)")
-                .font(.headline)
-                .padding()
+            Spacer().frame(height: 50)
             
-            Button(stopwatch.isRunning ? "Stop" : "Start") {
-                if stopwatch.isRunning {
-                    stopwatch.stop()
-                } else {
-                    stopwatch.start()
-                }
-            }
-            .font(.headline)
-            .padding()
+            TimerView(setDate: toDate)
         }
+        .font(.system(size: 30))
     }
 }
 
@@ -38,3 +57,31 @@ struct CountdownView_Previews: PreviewProvider {
         temporaryCountdownView()
     }
 }
+
+// MARK: - The stopwatch model is not used
+
+//struct temporaryCountdownView: View {
+//    @ObservedObject var stopwatch = Stopwatch()
+//
+//    var body: some View {
+//        VStack {
+//            Text("Countdown: \(stopwatch.remainingSeconds / 3600) hours, \((stopwatch.remainingSeconds % 3600) / 60) minutes, \(stopwatch.remainingSeconds % 60) seconds")
+//                .font(.headline)
+//                .padding()
+//
+//            Text("Stopwatch: \(stopwatch.message)")
+//                .font(.headline)
+//                .padding()
+//
+//            Button(stopwatch.isRunning ? "Stop" : "Start") {
+//                if stopwatch.isRunning {
+//                    stopwatch.stop()
+//                } else {
+//                    stopwatch.start()
+//                }
+//            }
+//            .font(.headline)
+//            .padding()
+//        }
+//    }
+//}
