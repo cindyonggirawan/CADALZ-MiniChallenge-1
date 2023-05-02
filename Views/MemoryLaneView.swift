967b612baf7063a430b7ce0e5411105cd2e07b74
daniel-bss <daniel.simamora2000@gmail.com>
2023-05-02 13:29:45 +0700

    add new Memory Lane View file

---
+++ Views/MemoryLaneView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoryLaneView.swift
+//  Playdate
+//
+//  Created by Daniel Bernard Sahala Simamora on 02/05/23.
+//
+
+import SwiftUI
+
+struct MemoryLaneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoryLaneView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoryLaneView()
+    }
+}
