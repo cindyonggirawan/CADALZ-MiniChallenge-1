795ee07210e2c2a99abe8c45bb59606e4eea0b58
Louis Wijaya <70441261+louis-not@users.noreply.github.com>
2023-05-03 15:12:22 +0700

    fix VM photopicker submit memory

--- Playdate/Features/Challenge/Presenters/Views/ChallengeSubmitModal.swift
+++ Playdate/Features/Challenge/Presenters/Views/ChallengeSubmitModal.swift
@@ -12,8 +12,11 @@ struct ChallengeSubmitModal: View {
     @Binding var isLikeChallenge: Bool
     @State var momentDescription = ""
     @State var navChallengeModal = 1
-    @State var selectedItems: [PhotosPickerItem] = []
+//    @State var selectedItems: [PhotosPickerItem] = []
+    @State private var selectedItems: PhotosPickerItem?
     @State var data: Data?
+    @State private var selectedImage: Image?
+    @State private var showImage: Bool = false
     
     var body: some View {
         NavigationView {
@@ -29,7 +32,6 @@ struct ChallengeSubmitModal: View {
                     
                     PhotosPicker (
                         selection: $selectedItems,
-                        maxSelectionCount: 1,
                         matching: .images
                     ) {
                         ZStack {
@@ -50,32 +52,49 @@ struct ChallengeSubmitModal: View {
                                     .stroke(style: StrokeStyle(lineWidth: 1, dash: [4]))
                                     .foregroundColor(Color.primaryDarkGray)
                             )
-                            
-                            if let data = data, let uiimage = UIImage(data: data) {
-                                Image(uiImage: uiimage)
-                                    .resizable()
-                            }
+//                            if let data = data, let uiimage = UIImage(data: data) {
+//                                Image(uiImage: uiimage)
+//                                    .resizable()
+//                            }
                         }
                     }
-                    .onChange(of: selectedItems) { newValue in
-                        guard let item = selectedItems.first else {
-                            return
-                        }
-                        item.loadTransferable(type: Data.self) { result in
-                            switch result {
-                            case .success(let data):
-                                if let data = data {
-                                    self.data = data
-                                } else {
-                                    print("Data is nil")
+//                    .onChange(of: selectedItems) { newValue in
+//                        guard let item = selectedItems.first else {
+//                            return
+//                        }
+//                        item.loadTransferable(type: Data.self) { result in
+//                            switch result {
+//                            case .success(let data):
+//                                if let data = data {
+//                                    self.data = data
+//                                } else {
+//                                    print("Data is nil")
+//                                }
+//                            case .failure(let failure):
+//                                fatalError("\(failure)")
+//                            }
+//                        }
+//                    }
+                    // TRIAL
+                    .onChange(of: selectedItems) { _ in
+                        Task {
+                            if let data = try? await selectedItems?.loadTransferable(type: Data.self) {
+                                if let uiImage = UIImage(data: data) {
+                                    showImage = true
+                                    selectedImage = Image(uiImage: uiImage)
+                                        .resizable()
+                                    return
                                 }
-                            case .failure(let failure):
-                                fatalError("\(failure)")
                             }
+                            print("failed")
                         }
                     }
                 }
                 .padding(.bottom, 20)
+                
+                if (showImage){
+                    selectedImage
+                }
                 
                 VStack(alignment: .leading) {
                     Text("Write about this moment")
@@ -94,7 +113,9 @@ struct ChallengeSubmitModal: View {
                 Button(action: {
                     //TODO: send isLike data to firebase
 //                    FBaddLikeToChallenge()
-//                    pake function ini buat simpen ke memory -> memoryViewModel.submitMemory(photo: <#T##UIImage#>, description: <#T##String#>) (BELUM DI TESTING SAMA SEKALI)
+//                    pake function ini buat simpen ke memory ->
+//                    MemoryViewModel.submitMemory(photo: URL(resolvingBookmarkData: uiimage, bookmarkDataIsStale: &<#T##Bool#>), description: momentDescription)
+                    
                 }, label: {
                     Text("Submit")
                         .font(.custom("Poppins-Bold", size: 14))
