e45c58f70fbc8811f7b7c4100cfaf55b5f1b1127
daniel-bss <daniel.simamora2000@gmail.com>
2023-05-05 17:02:23 +0700

    add new capsule view for MemoryLane

---
+++ Playdate/Features/Memory/Presenters/Components/CategoryCapsuleViewDua.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCapsuleViewDua.swift
+//  Playdate
+//
+//  Created by Daniel Bernard Sahala Simamora on 05/05/23.
+//
+
+import SwiftUI
+
+struct CategoryCapsuleViewDua: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryCapsuleViewDua_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryCapsuleViewDua()
+    }
+}
